a1753edf5b89f7848ee04dffe5066b705a310af1
Roisin Yates <roisinyates@outlook.com>
2024-07-21 00:51:10 +1200

    Add router for content card link

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,38 @@ import { Header } from './Components/Header/Header'
 import { Carousel } from './Components/Carousel/Carousel'
 import { ContentContainer } from './Components/MainContents/ContentContainer'
 
-const App = () => {
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom'
+
+const Layout = () => {
   return (
     <div className="App">
       <Header />
       <div id="main" role="main">
-        <Carousel />
-        <ContentContainer />
+        <Outlet />
       </div>
     </div>
+  )
+}
+
+const Index = () => {
+  return (
+    <>
+      <Carousel />
+      <ContentContainer />
+    </>
+  )
+}
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Index />} />
+          <Route path="*" element={<p>The link works!</p>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
